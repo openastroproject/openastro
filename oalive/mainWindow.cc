7cd2c36791db34765dc83ade67000c8be71926c8
James Fidell <james@openastroproject.org>
2018-09-09 10:32:32 +0100

    Correct typos

--- oalive/mainWindow.cc
+++ oalive/mainWindow.cc
@@ -322,18 +322,18 @@ MainWindow::readConfig ( QString configFile )
     settings = new QSettings ( configFile, QSettings::IniFormat );
   } else {
 #if defined(__APPLE__) && defined(__MACH__) && TARGET_OS_MAC == 1
-    QSettings iniSettings = new QSettings ( QSettings::IniFormat,
+    QSettings* iniSettings = new QSettings ( QSettings::IniFormat,
         QSettings::UserScope, ORGANISATION_NAME_SETTINGS, APPLICATION_NAME );
-    settings = &iniSettings;
+    settings = iniSettings;
 
     // The intention here is to allow a new ini-format file to override an
     // old plist file, but to use the plist file if no other exists
-    if ( iniSettings.value ( "saveSettings", -1 ).toInt() == -1 ) {
-      QSettings plistSettings = new QSettings ( ORGANISATION_NAME_SETTINGS,
+    if ( iniSettings->value ( "saveSettings", -1 ).toInt() == -1 ) {
+      QSettings* plistSettings = new QSettings ( ORGANISATION_NAME_SETTINGS,
           APPLICATION_NAME );
-      if ( plistSettings.value ( "saveSettings", -1 ).toInt() != -1 ) {
+      if ( plistSettings->value ( "saveSettings", -1 ).toInt() != -1 ) {
         delete iniSettings;
-        settings = &plistSettings;
+        settings = plistSettings;
       } else {
         delete plistSettings;
       }
