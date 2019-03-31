83a63d682c27c61e606da478ef2504365a3cd2b1
James Fidell <james@openastroproject.org>
2019-03-31 00:34:14 +0000

    Start of dummy camera implementation.  #55

---
+++ liboacam/dummy/dummyoacam.c
@@ -0,0 +1,79 @@
+/*****************************************************************************
+ *
+ * dummyoacam.c -- main entrypoint for dummy camera
+ *
+ * Copyright 2019 James Fidell (james@openastroproject.org)
+ *
+ * License:
+ *
+ * This file is part of the Open Astro Project.
+ *
+ * The Open Astro Project is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Open Astro Project is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Open Astro Project.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+#include <oa_common.h>
+#include <openastro/camera.h>
+
+#include "unimplemented.h"
+#include "oacamprivate.h"
+#include "dummyoacam.h"
+
+
+int
+oaDummyGetCameras ( CAMERA_LIST* deviceList, int flags )
+{
+  unsigned int		numFound = 0, i;
+  int							ret;
+  const char*			currName;
+  oaCameraDevice*	dev[2];
+  DEVICE_INFO*		_private[2];
+  int							j, cameraType, found;
+
+	// Create two cameras -- one for planetary and one for DSO
+
+	if (!( dev = calloc ( sizeof ( oaCameraDevice ), 2 ))) {
+    return -OA_ERR_MEM_ALLOC;
+  }
+
+  if (!( _private = calloc ( sizeof ( DEVICE_INFO ), 2 ))) {
+    ( void ) free (( void* ) dev );
+    return -OA_ERR_MEM_ALLOC;
+  }
+
+  _oaInitCameraDeviceFunctionPointers ( dev[0] );
+  _oaInitCameraDeviceFunctionPointers ( dev[1] );
+
+  dev[0]->interface = dev[1]->interface = OA_CAM_IF_DUMMY;
+  _private[0]->devType = _private[1]->devType = 0;
+  _private[0]->devIndex = _private[1]->devIndex = 0;
+	( void ) strncpy ( dev[0]->deviceName, "Planetary cam", OA_MAX_NAME_LEN );
+	( void ) strncpy ( dev[1]->deviceName, "DSO cam", OA_MAX_NAME_LEN );
+  dev[0]->_private = _private[0];
+  dev[1]->_private = _private[1];
+  dev[0]->initCamera = dev[1]->initCamera = oaDummyInitCamera;
+  dev[0]->hasLoadableFirmware = dev[1]->hasLoadableFirmware = 0;
+
+	for ( i = 0; i < 2; i++ ) {
+    if (( ret = _oaCheckCameraArraySize ( deviceList )) < 0 ) {
+      ( void ) free (( void* ) dev );
+      ( void ) free (( void* ) _private );
+      return ret;
+    }
+    deviceList->cameraList[ deviceList->numCameras++ ] = dev[i];
+  }
+
+  return 2;
+}
---
+++ liboacam/dummy/dummyoacam.h
@@ -0,0 +1,45 @@
+/*****************************************************************************
+ *
+ * dummyoacam.h -- header for dummy camera
+ *
+ * Copyright 2019 James Fidell (james@openastroproject.org)
+ *
+ * License:
+ *
+ * This file is part of the Open Astro Project.
+ *
+ * The Open Astro Project is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Open Astro Project is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Open Astro Project.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+#ifndef DUMMY_OACAM_H
+#define DUMMY_OACAM_H
+
+extern int		oaDummyGetCameras ( CAMERA_LIST*, int );
+extern oaCamera*	oaDummyInitCamera ( oaCameraDevice* );
+extern int              oaDummyCloseCamera ( oaCamera* );
+
+extern int		oaDummyCameraTestControl ( oaCamera*, int,
+				oaControlValue* );
+extern int		oaDummyCameraReadControl ( oaCamera*, int,
+				oaControlValue* );
+extern int		oaDummyCameraGetControlRange ( oaCamera*, int,
+				int64_t*, int64_t*, int64_t*, int64_t* );
+
+extern void*		oacamDummycontroller ( void* );
+
+extern int		oaDummyCameraGetFramePixelFormat ( oaCamera* );
+
+#endif	/* DUMMY_OACAM_H */
