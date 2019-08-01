8176f310f21872e6d8c728bbdabed46907daed92
James Fidell <james@openastroproject.org>
2019-08-02 00:38:25 +0100

    Add missing file from previous edit.  #408

---
+++ liboacam/gphoto2/GP2control.c
@@ -0,0 +1,147 @@
+/*****************************************************************************
+ *
+ * GP2control.c -- control functions for libgphoto2 cameras
+ *
+ * Copyright 2019
+ *   James Fidell (james@openastroproject.org)
+ *
+ * License:
+ *
+ * This file is part of the Open Astro Project.
+ *
+ * The Open Astro Project is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Open Astro Project is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Open Astro Project.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+#include <oa_common.h>
+
+#include <pthread.h>
+#include <openastro/camera.h>
+#include <openastro/util.h>
+
+#include "oacamprivate.h"
+#include "GP2oacam.h"
+#include "GP2state.h"
+
+
+int
+oaGP2CameraSetControl ( oaCamera* camera, int control, oaControlValue* val,
+  int dontWait )
+{
+  OA_COMMAND	command;
+  GP2_STATE*	cameraInfo = camera->_private;
+  int		retval = OA_ERR_NONE;
+
+  // Could do more validation here, but it's a bit messy to do here
+  // and in the controller too.
+
+  OA_CLEAR ( command );
+  command.commandType = OA_CMD_CONTROL_SET;
+  command.controlId = control;
+  command.commandData = val;
+
+  cameraInfo = camera->_private;
+  oaDLListAddToTail ( cameraInfo->commandQueue, &command );
+  pthread_cond_broadcast ( &cameraInfo->commandQueued );
+  if ( !dontWait ) {
+    pthread_mutex_lock ( &cameraInfo->commandQueueMutex );
+    while ( !command.completed ) {
+      pthread_cond_wait ( &cameraInfo->commandComplete,
+          &cameraInfo->commandQueueMutex );
+    }
+    pthread_mutex_unlock ( &cameraInfo->commandQueueMutex );
+    retval = command.resultCode;
+  }
+
+  return retval;
+}
+
+
+int
+oaGP2CameraReadControl ( oaCamera* camera, int control, oaControlValue* val )
+{
+  OA_COMMAND    command;
+  GP2_STATE*   cameraInfo = camera->_private;
+  int           retval;
+
+  // Could do more validation here, but it's a bit messy to do here
+  // and in the controller too.
+
+  OA_CLEAR ( command );
+  command.commandType = OA_CMD_CONTROL_GET;
+  command.controlId = control;
+  command.resultData = val;
+
+  cameraInfo = camera->_private;
+  oaDLListAddToTail ( cameraInfo->commandQueue, &command );
+  pthread_cond_broadcast ( &cameraInfo->commandQueued );
+  pthread_mutex_lock ( &cameraInfo->commandQueueMutex );
+  while ( !command.completed ) {
+    pthread_cond_wait ( &cameraInfo->commandComplete,
+        &cameraInfo->commandQueueMutex );
+  }
+  pthread_mutex_unlock ( &cameraInfo->commandQueueMutex );
+  retval = command.resultCode;
+
+  return retval;
+}
+
+
+int
+oaGP2CameraTestControl ( oaCamera* camera, int control, oaControlValue* val )
+{
+  uint32_t	val_u32;
+  int64_t	val_s64;
+  COMMON_INFO*	commonInfo = camera->_common;
+
+  if ( !camera->OA_CAM_CTRL_TYPE( control )) {
+    return -OA_ERR_INVALID_CONTROL;
+  }
+
+  if ( camera->OA_CAM_CTRL_TYPE( control ) != val->valueType ) {
+    return -OA_ERR_INVALID_CONTROL_TYPE;
+  }
+
+  switch ( control ) {
+
+    case OA_CAM_CTRL_ISO:
+    case OA_CAM_CTRL_WHITE_BALANCE:
+    case OA_CAM_CTRL_SHUTTER_SPEED:
+    case OA_CAM_CTRL_SHARPNESS:
+      // This should be unsigned 32-bit
+      val_s64 = val->int64;
+      if ( val_s64 < 0 ) {
+        return -OA_ERR_OUT_OF_RANGE;
+      }
+      val_u32 = val_s64 & 0xffffffff;
+      if ( val_u32 >= commonInfo->OA_CAM_CTRL_MIN( control ) &&
+          val_u32 <= commonInfo->OA_CAM_CTRL_MAX( control ) &&
+          ( 0 == ( val_u32 - commonInfo->OA_CAM_CTRL_MIN( control )) %
+          commonInfo->OA_CAM_CTRL_STEP( control ))) {
+        return OA_ERR_NONE;
+      }
+      break;
+
+    default:
+      // If we reach here it's because we don't recognise the control
+      fprintf ( stderr, "Unrecognised control %d in %s\n", control,
+          __FUNCTION__ );
+      return -OA_ERR_INVALID_CONTROL;
+      break;
+  }
+
+  // And if we reach here it's because the value wasn't valid
+  return -OA_ERR_OUT_OF_RANGE;
+}
