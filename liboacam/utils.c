6d24203602685b3ec91aecb19d8a1d8a102b0713
James Fidell <james@openastroproject.org>
2019-08-24 21:26:41 +0100

    Start on refactoring code for initialising camera data structures.

--- liboacam/utils.c
+++ liboacam/utils.c
@@ -2,7 +2,8 @@
  *
  * utils.c -- random support functions for cameras
  *
- * Copyright 2014,2015,2018 James Fidell (james@openastroproject.org)
+ * Copyright 2014,2015,2018,2019
+ *   James Fidell (james@openastroproject.org)
  *
  * License:
  *
@@ -28,6 +29,7 @@
 #include <openastro/camera.h>
 
 #include "oacamprivate.h"
+#include "unimplemented.h"
 
 
 void
@@ -66,4 +68,48 @@ _oaCheckCameraArraySize ( CAMERA_LIST* deviceList )
   deviceList->cameraList = newList;
   deviceList->maxCameras = newNum;
   return OA_ERR_NONE;
+}
+
+
+int
+_oaInitCameraStructs ( oaCamera** camera, void** state, size_t stateSize,
+		COMMON_INFO** common )
+{
+	oaCamera*			p_camera;
+	void*					p_state;
+	COMMON_INFO*	p_common;
+
+	if (!( p_camera = ( oaCamera* ) malloc ( sizeof ( oaCamera )))) {
+		perror ( "malloc of oaCamera struct failed" );
+		return -OA_ERR_MEM_ALLOC;
+	}
+	*camera = p_camera;
+
+	if (!( p_state = malloc ( stateSize ))) {
+		perror ( "malloc of camera state struct failed" );
+		free (( void* ) p_camera );
+		*camera = 0;
+		return -OA_ERR_MEM_ALLOC;
+	}
+	*state = p_state;
+
+	if (!( p_common = ( COMMON_INFO* ) malloc ( sizeof ( COMMON_INFO )))) {
+		perror ( "malloc of COMMON_INFO struct failed" );
+		free (( void* ) p_camera );
+		free ( p_state );
+		*camera = 0;
+		*state = 0;
+		return -OA_ERR_MEM_ALLOC;
+	}
+	*common = p_common;
+
+	memset ( p_camera, 0, sizeof ( oaCamera ));
+	memset ( p_state, 0, stateSize );
+	memset ( p_common, 0, sizeof ( COMMON_INFO ));
+	p_camera->_private = p_state;
+	p_camera->_common = p_common;
+
+	_oaInitCameraFunctionPointers ( p_camera );
+
+	return OA_ERR_NONE;
 }
