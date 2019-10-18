7ade9e6053d0b25857df85005d2d9218a4f8d7a6
James Fidell <james@openastroproject.org>
2019-10-18 19:26:41 +0100

    Remove reference to redundant SetROI functions

--- liboacam/touptek/connect.c
+++ liboacam/touptek/connect.c
@@ -702,7 +702,6 @@ TT_FUNC( _, InitFunctionPointers )( oaCamera* camera )
   camera->funcs.getControlDiscreteSet =
 		TT_FUNC( oa, CameraGetControlDiscreteSet );
 
-  camera->funcs.setROI = TT_FUNC( oa, CameraSetROI );
   camera->funcs.testROISize = TT_FUNC( oa, CameraTestROISize );
 
   camera->funcs.hasAuto = oacamHasAuto;
