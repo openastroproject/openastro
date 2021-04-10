ccaea1c7a122c1ff0ab003948e885b6611e61af6
James Fidell <james@openastroproject.org>
2021-04-11 00:03:34 +0100

    Remove redundant variables

--- liboacam/spinnaker/Spinstate.h
+++ liboacam/spinnaker/Spinstate.h
@@ -45,37 +45,8 @@ typedef struct SPINNAKER_STATE {
 	double			maxFloatBlacklevel;
 	double			minFloatGamma;
 	double			maxFloatGamma;
-
-  // pointers to shared library functions so we can use them if they are
-  // present
-
-  void*			p_spinSystemGetInstance;
-  void*			p_spinCameraListClear;
-  void*			p_spinCameraListCreateEmpty;
-  void*			p_spinCameraListDestroy;
-  void*			p_spinCameraListGetSize;
-  void*			p_spinInterfaceListClear;
-  void*			p_spinInterfaceListCreateEmpty;
-  void*			p_spinInterfaceListDestroy;
-  void*			p_spinInterfaceListGetSize;
-  void*			p_spinSystemGetCameras;
-  void*			p_spinSystemGetInterfaces;
-  void*			p_spinSystemReleaseInstance;
-  void*			p_spinInterfaceListGet;
-  void*			p_spinInterfaceRelease;
-  void*			p_spinInterfaceGetTLNodeMap;
-  void*			p_spinNodeMapGetNode;
-  void*			p_spinNodeIsAvailable;
-  void*			p_spinNodeIsReadable;
-  void*			p_spinStringGetValue;
-  void*			p_spinIntegerGetValue;
-  void*			p_spinEnumerationEntryGetEnumValue;
-  void*			p_spinEnumerationGetCurrentEntry;
-  void*			p_spinInterfaceGetCameras;
-  void*			p_spinCameraListGet;
-  void*			p_spinCameraGetTLDeviceNodeMap;
-  void*			p_spinCameraRelease;
-
+	double			minFloatHue;
+	double			maxFloatHue;
 } SPINNAKER_STATE;
 
 #endif	/* OA_SPINNAKER_STATE_H */
